738ceb678e9b7f5206c430137afba9da344d3a35
ErenOg <79149642+ErenOg@users.noreply.github.com>
2021-02-17 23:17:14 +0300

    Waiting Room Scene Added

---
+++ GatesWinterJam/Assets/DialogueManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    public GameObject dialogueNextButton;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+    void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            dialogueNextButton.SetActive(true);
+        }
+    }
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+    public void NextSentence()
+    {
+        dialogueNextButton.SetActive(false);
+        if(index < sentences.Length -1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+    }
+}
